b4805d1056405e831b317d0e524e39c87f515975
xuegeng <xg1375747987@163.com>
2019-05-30 22:14:05 +0800

    05

---
+++ Spring05/src/com/xupt/TextEditor.java
@@ -0,0 +1,28 @@
+package com.xupt;
+
+public class TextEditor {
+	
+	private SpellChecker spellChecker;
+	private String name;
+	   
+	   
+	   public void setSpellChecker( SpellChecker spellChecker ){
+	      this.spellChecker = spellChecker;
+	   }
+	   
+	   public SpellChecker getSpellChecker() {
+	      return spellChecker;
+	   }
+	   
+	   public void setName(String name) {
+	      this.name = name;
+	   }
+	   
+	   public String getName() {
+	      return name;
+	   }
+	   
+	   public void spellCheck() {
+	      spellChecker.checkSpelling();
+	   }
+}
