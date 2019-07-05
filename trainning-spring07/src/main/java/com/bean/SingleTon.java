9073781c0ac10ea559ffd7beb209d0c49e5431a6
xuegeng <xg1375747987@163.com>
2019-07-05 14:37:00 +0800

    t7

---
+++ trainning-spring07/src/main/java/com/bean/SingleTon.java
@@ -0,0 +1,28 @@
+package com.bean;
+
+public class SingleTon {
+
+	private SingleTon()
+	{}
+	
+	private static SingleTon st;
+	
+	
+	public static SingleTon getInstance()
+	{
+		if(null==st)
+			st=new SingleTon();
+		
+		return st;
+	}
+	
+	public static void main(String[] args) {
+		 
+		SingleTon s1  = SingleTon.getInstance();
+		SingleTon s2  = SingleTon.getInstance();
+		
+		
+		System.out.println(s1+"--"+s2);
+	}
+
+}
